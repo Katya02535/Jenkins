d334e196a2aaec1850c50d514f0f0314d7e5fa03
Katya02535 <melkotrva83@gmail.com>
2021-07-21 21:31:21 -0400

    second commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,7 +2,7 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println("Batch 9 is great");
             Thread.sleep(1000);
         }
